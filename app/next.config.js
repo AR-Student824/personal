2b76854f27f06bb5c072902fd7e6252c02f04e21
AR <techrebel2723@gmail.com>
2021-07-11 20:49:45 +0000

    fix redirects

--- app/next.config.js
+++ app/next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/to/tua',
+        destination: 'https://totallyusefulapi.ml',
+        permanent: true,
+      },
+      {
+        source: '/to/hgle',
+        destination: 'https://hangle.me',
+        permanent: true,
+      },
+    ]
+  }
 }
